a45699b3178bb2548b17494310e370992f7ba913
Adam Conrad <acconrad@protonmail.com>
2019-07-20 02:46:57 -0400

    Tie in retry logic to server package

--- src/middleware/health.ts
+++ src/middleware/health.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 
 import { Initializable } from '@indigo-ag/common';
 
-import { MIDDLEWARE_READY_FUNC } from '../core/server';
+import { APPLICATION_READY_FUNC } from '../core/server';
 
 export type HealthConfig = {
   manifest: string;
@@ -11,14 +11,14 @@ export type HealthConfig = {
 const APPLICATION_ERROR_MSG = 'Application not ready';
 
 export class Health implements Initializable<express.Application> {
-  constructor(private readonly config: HealthConfig) { }
+  constructor(private readonly config: HealthConfig) {}
 
   initialize(app: express.Application) {
-    const isMiddlewareReady = app.get(MIDDLEWARE_READY_FUNC);
+    const isApplicationReady = app.get(APPLICATION_READY_FUNC);
 
     const health = (req: express.Request, res: express.Response) => {
       // determine if application is ready to accept requests
-      if (!isMiddlewareReady()) {
+      if (!isApplicationReady()) {
         res.status(500).send({ data: APPLICATION_ERROR_MSG });
         return;
       }
